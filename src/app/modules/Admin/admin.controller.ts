af5125721f018fc620425dcdfa8ffc47bfeb2e2d
samimhossainsujon <samimhossainsanto@gmail.com>
2024-03-20 02:14:29 +0600

    filter data add

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -2,7 +2,8 @@ import { Request, Response } from "express";
 import { AdminService } from "./admin.service";
 
 const getAllFromDB = async (req: Request, res: Response) => {
-  const result = await AdminService.getAllFromDB();
+  // console.log(req.query);
+  const result = await AdminService.getAllFromDB(req.query);
   res.status(200).json({
     success: true,
     message: "Admin Data Fetched successfully",
--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -1,11 +1,42 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
-const getAllFromDB = async () => {
-    const result = await prisma.admin.findMany();
-    return result
-}
+const getAllFromDB = async (params: any) => {
+  const { searchTerm, ...filterData } = params;
+  const andCondions: Prisma.AdminWhereInput[] = [];
+  const adminSearchAbleFields = ["name", "email"];
+
+  console.log(filterData);
+  if (params.searchTerm) {
+    andCondions.push({
+      OR: adminSearchAbleFields.map((filed) => ({
+        [filed]: {
+          contains: params.searchTerm,
+          mode: "insensitive",
+        },
+      })),
+    });
+  }
+
+  if (Object.keys(filterData).length>0) {
+    andCondions.push({
+      AND: Object.keys(filterData).map(key => ({
+        [key]: {
+            equals :filterData[key]
+          }
+        }))
+      })
+  }
+
+  console.dir(andCondions, { depth: "infinity" });
+
+  const WhereConditions: Prisma.AdminWhereInput = { AND: andCondions };
+  const result = await prisma.admin.findMany({
+    where: WhereConditions,
+  });
+  return result;
+};
 
 export const AdminService = {
-    getAllFromDB
-}
+  getAllFromDB,
+};
